41c559c5916fcd9c590c3294c375dc60cb096f9d
Miguel Salazar <miguelsalazar88@gmail.com>
2024-06-11 17:36:45 -0500

    create by_store endpoint

--- app/routers/sales.py
+++ app/routers/sales.py
@@ -22,9 +22,27 @@ def get_sales_by_employee(key_employee: int, start_date: str, end_date: str):
 
 @router.get("/by_product")
 def get_sales_by_product(key_product: int, start_date: str, end_date: str):
+
     df = get_sales_data()
+
+    key_product = formatting.add_pipe(key_product)
+
     result = df[(df["KeyProduct"] == key_product) & 
                 (df["KeyDate"] >= start_date) &
                 (df["KeyDate"] <= end_date)]
     return result.to_dict(orient="records")
+
+@router.get("/by_store")
+def get_sales_by_store(key_store: str, start_date: str, end_date: str):
+    df = get_sales_data()
+
+    key_store = formatting.add_pipe(key_store)
+
+    result = df[(df["KeyStore"] == key_store) & 
+                (df["KeyDate"] >= start_date) &
+                (df["KeyDate"] <= end_date)]
+    return result.to_dict(orient="records")
+
+
+
     
